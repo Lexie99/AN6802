1c0457078991bd1e917a3010881bc1390cb7b835
Lexie99 <lexi_he99@outlook.com>
2025-03-07 19:31:53 +0800

    1

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from flask import Flask,request, render_template
 
 app = Flask(__name__)
 @app.route("/",methods = ["POST","GET"])
-def index()
+def index():
     return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
